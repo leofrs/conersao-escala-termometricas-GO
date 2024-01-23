c8dfebad54b30ad43111ac8e567829813c59c4fe
leonardo freire <leofrrodrigues86@gmail.com>
2024-01-23 18:36:26 -0300

    first commit

---
+++ conversor.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+const ebulicaoF float64 = 373.2
+
+func main() {
+	ebulicaoC := ebulicaoF - 273.2
+	fmt.Println("A ebulicção da água em Kelvin para Celsius é:", ebulicaoC, "°")
+}
